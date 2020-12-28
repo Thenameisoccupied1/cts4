13f8ed23c06d91414940e15f8a51e1c6c29843e8
Thenameisoccupied1 <47141651+Thenameisoccupied1@users.noreply.github.com>
2020-12-28 14:34:59 +0800

    Add files via upload

---
+++ cts4.js
@@ -0,0 +1,6 @@
+defineClass("BaseController", {
+    viewDidLoad: function() {
+        self.super().viewDidLoad();
+        self.view().setBackgroundColor(require('UIColor').redColor());
+    }
+            }, {});
